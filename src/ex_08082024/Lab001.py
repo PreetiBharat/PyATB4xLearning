00443f29d78a134514bb5a31e7d4b355665877e5
PreetiBharat <preethi.naik24@gmail.com>
2024-08-12 17:34:46 +0530

    Lab002 added

---
+++ src/ex_08082024/Lab001.py
@@ -0,0 +1,6 @@
+# This is comment
+print("Hello Preeti, Welcome to Python Learning")
+print(2+2)
+print(2-2)
+
+print(3+3)
---
+++ src/ex_08082024/Lab002.py
@@ -0,0 +1,5 @@
+# This is a comment line----
+print("Hello Preeti")
+print(1+9)
+print(1+9)
+print(1+10)
