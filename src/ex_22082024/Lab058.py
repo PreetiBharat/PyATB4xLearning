dd9faac711407ba9d5058a6c8f0ee99ed8e09d67
PreetiBharat <preethi.naik24@gmail.com>
2024-08-26 22:49:28 +0530

    Committed programs of Aug 20

---
+++ src/ex_22082024/Lab058.py
@@ -0,0 +1,23 @@
+# continue statement
+# continue statement skips the current iteration of a loop and
+# moves to the next iteration.
+
+for number in range(1, 10):
+    if number % 2 == 0:
+        print(number)  # this prints even numbers
+    else:
+        continue
+
+for number in range(1, 10):
+    if number % 2 == 0:
+        continue  # # this prints odd numbers
+    else:
+        print(number)
+
+# | i  | Condition | O/P
+# | 0  | 0%2 == 0 -> True | continue - skip No O/P
+# | 1  | 1%2 == 0 -> False | 1
+# | 2  | 1%2 == 0 -> True | continue - skip No O/P
+# | 3  | 3%2 == 0 -> False | 3
+
+# pass - can be used in the statement, functions, class and objects
---
+++ src/ex_22082024/Lab059.py
@@ -0,0 +1,27 @@
+# Match Statement
+# Its called Switch in Java
+# match the o/p and execute
+# This works only when Python version > 3.10
+
+# match variable:
+#     case pattern1:
+#         # code block
+#     case pattern2:
+#         # code block
+
+# Write a program to ask the user which browser he wants to run automation
+browser_name = input("Enter the name of the browser\n")
+browser_name = browser_name.lower()
+match browser_name:
+    case "firefox":
+        if browser_name == "firefox":
+            print("Hello Hello")
+        print("Execute the firefox Code")
+    case "chrome":
+        print("Execute the Chrome Code")
+    case "edge":
+        print("Execute the Edge Code")
+    case "safari":
+        print("Execute the Safari Code")
+    case _:
+        print("Browser Not Found!")
